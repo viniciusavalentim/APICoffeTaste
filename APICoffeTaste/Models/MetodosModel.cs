d1886beba0508a6619c0c4712546800f93903405
Vinicius <viniciusvalentim769@gmail.com>
2024-01-15 18:02:33 -0300

    Inserindo Metodos model

---
+++ APICoffeTaste/Models/MetodosModel.cs
@@ -0,0 +1,11 @@
+﻿namespace APICoffeTaste.Models
+{
+    public class MetodosModel
+    {
+        public string? Metodos { get; set; }
+        public string? Variacao { get; set; }
+        public float QuantidadeDeCafe { get; set; }
+        public float QuantidadeDeAgua { get; set; } 
+        public int Temperatura { get; set; }
+    }
+}
