90972bd21e1b051108834c1b89d0b30a88bc5d75
Vinicius <viniciusvalentim769@gmail.com>
2024-04-05 23:56:00 -0300

    Criando as relações entre hotdrinlksmodel com ingredientshotdrinlksmodel

--- APICoffeTaste/Models/HotDrinksModel.cs
+++ APICoffeTaste/Models/HotDrinksModel.cs
@@ -8,5 +8,7 @@ namespace APICoffeeTaste.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Observacoes { get; set; }
+        public List<IngredientsHotDrinksModel>? Ingredientes { get; set; }
+
     }
 }
---
+++ APICoffeTaste/Models/IngredientsHotDrinksModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace APICoffeeTaste.Models
+{
+    public class IngredientsHotDrinksModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Quantity { get; set; }
+        public string? Unit { get; set; }
+        public int HotDrinksId { get; set; }
+        [JsonIgnore]
+        public HotDrinksModel? HotDrinks { get; set; }
+    }
+}
