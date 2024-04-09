e17d1bc5a0556945c73f9019b100a6d3e5c8655d
Vinicius <viniciusvalentim769@gmail.com>
2024-04-09 08:16:16 -0300

    Criando a service e a interface do "CoffeeSprintsModel"

--- APICoffeTaste/Service/CoffeeSprintsService/ICoffeeSprintsInterface.cs
+++ APICoffeTaste/Service/CoffeeSprintsService/ICoffeeSprintsInterface.cs
@@ -1,7 +1,16 @@
-﻿namespace APICoffeeTaste.Service.CoffeeSprintsService
+﻿using APICoffeeTaste.Dtos;
+using APICoffeeTaste.Models;
+
+namespace APICoffeeTaste.Service.CoffeeSprintsService
 {
     public interface ICoffeeSprintsInterface
     {
-
+        Task<ServiceResponse<List<CoffeeSprintsModel>>> GetCoffeeSprints();
+        Task<ServiceResponse<List<CoffeeSprintsModel>>> CreateCoffeeSprints(DtoCreateCoffeeSprints createTeas);
+        Task<ServiceResponse<List<CoffeeSprintsModel>>> UpdateCoffeeSprints(CoffeeSprintsModel updateTea);
+        Task<ServiceResponse<List<CoffeeSprintsModel>>> DeleteCoffeeSprints(int id);
+        Task<ServiceResponse<CoffeeSprintsModel>> GetCoffeeSprintsById(int id);
+        Task<ServiceResponse<List<IngredientsCoffeeSprintsModel>>> GetIngredientsByCoffeeSprintId(int id);
     }
+}
 }
